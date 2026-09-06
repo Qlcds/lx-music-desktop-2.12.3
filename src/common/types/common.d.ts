b9f6772e42fc6a76411243c232c835db82456d39
QLOJI <56267846+QLOJI@users.noreply.github.com>
2026-09-06 19:51:58 +0800

    Add files via upload

--- src/common/types/common.d.ts
+++ src/common/types/common.d.ts
@@ -47,7 +47,7 @@ declare namespace LX {
 
   type OnlineSource = 'kw' | 'kg' | 'tx' | 'wy' | 'mg'
   type Source = OnlineSource | 'local'
-  type Quality = '128k' | '320k' | 'flac' | 'flac24bit' | '192k' | 'ape' | 'wav'
+  type Quality = '128k' | '320k' | 'flac' | 'flac24bit' | '192k' | 'ape' | 'wav' | 'atmos' | 'master'
 
   type QualityList = Partial<Record<LX.Source, LX.Quality[]>>
 
